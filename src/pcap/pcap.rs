1bda5e28abaf63f608e657df32784528313ed0e8
DrBrad <brad@bradeagle.com>
2025-03-11 08:48:58 -0600

    PCAP read/write

--- src/pcap/pcap.rs
+++ src/pcap/pcap.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io;
-use std::io::{ErrorKind, Read};
+use std::io::{ErrorKind, Read, Write};
 use std::vec::IntoIter;
 use crate::packet::inter::data_link_types::DataLinkTypes;
 use crate::packet::packet::Packet;
@@ -19,14 +19,6 @@ pub struct Pcap {
     packets: Vec<Packet>
 }
 
-#[derive(Debug)]
-pub struct PacketHeader {
-    timestamp_sec: u32,       // Timestamp seconds
-    timestamp_usec: u32,      // Timestamp microseconds
-    captured_len: u32,        // Length of captured packet
-    original_len: u32,        // Original packet length
-}
-
 impl Pcap {
 
     pub fn new() -> Self {
@@ -43,7 +35,7 @@ impl Pcap {
 
     pub fn from_file(file_path: &str) -> io::Result<Self> {
         let mut file = File::open(file_path)?;
-        let mut buf = [0u8; 24]; // Global header is 24 bytes
+        let mut buf = [0u8; 24];
         file.read_exact(&mut buf)?;
 
         if buf.len() < PCAP_HEADER_LEN {
@@ -70,17 +62,15 @@ impl Pcap {
                 break;
             }
 
-            let packet_header = PacketHeader {
-                timestamp_sec: u32::from_le_bytes([buf[0], buf[1], buf[2], buf[3]]),
-                timestamp_usec: u32::from_le_bytes([buf[4], buf[5], buf[6], buf[7]]),
-                captured_len: u32::from_le_bytes([buf[8], buf[9], buf[10], buf[11]]),
-                original_len: u32::from_le_bytes([buf[12], buf[13], buf[14], buf[15]]),
-            };
+            let timestamp_sec = u32::from_le_bytes([buf[0], buf[1], buf[2], buf[3]]);
+            let timestamp_usec = u32::from_le_bytes([buf[4], buf[5], buf[6], buf[7]]);
+            let captured_len = u32::from_le_bytes([buf[8], buf[9], buf[10], buf[11]]);
+            let frame_time = Self::combine_timestamp(timestamp_sec, timestamp_usec);
 
-            let mut buf = vec![0u8; packet_header.captured_len as usize];
+            let mut buf = vec![0u8; captured_len as usize];
             file.read_exact(&mut buf)?;
 
-            packets.push(Packet::new(data_link_type, 0, &buf));
+            packets.push(Packet::new(data_link_type, frame_time, &buf));
         }
 
         Ok(Self {
@@ -97,6 +87,23 @@ impl Pcap {
     pub fn to_file(&self, file_path: &str) -> io::Result<()> {
         let mut file = File::create(file_path)?;
 
+        file.write_all(&MAGIC_NUMBER.to_le_bytes())?;
+        file.write_all(&self.version_major.to_le_bytes())?;
+        file.write_all(&self.version_minor.to_le_bytes())?;
+        file.write_all(&self.zone.to_le_bytes())?;
+        file.write_all(&self.accuracy.to_le_bytes())?;
+        file.write_all(&self.payload_length.to_le_bytes())?;
+        file.write_all(&self.data_link_type.get_code().to_le_bytes())?;
+
+        for packet in &self.packets {
+            let (timestamp_sec, timestamp_usec) = Self::split_timestamp(packet.get_frame_time());
+            file.write_all(&timestamp_sec.to_le_bytes())?;
+            file.write_all(&timestamp_usec.to_le_bytes())?;
+            file.write_all(&packet.len().to_le_bytes())?;
+            file.write_all(&packet.len().to_le_bytes())?;
+
+            file.write_all(&packet.to_bytes())?;
+        }
 
         Ok(())
     }
@@ -115,6 +122,15 @@ impl Pcap {
 
     pub fn get_accuracy(&self) -> u32 {
         self.accuracy
+    }
+
+    pub fn compute_length(&self) -> usize {
+        let mut payload_length = 0;
+        for packet in &self.packets {
+            payload_length += packet.len();
+        }
+
+        self.payload_length as usize
     }
 
     pub fn get_payload_length(&self) -> usize {
@@ -139,6 +155,17 @@ impl Pcap {
 
     pub fn total_packets(&self) -> usize {
         self.packets.len()
+    }
+
+    fn split_timestamp(timestamp_ns: u128) -> (u32, u32) {
+        let timestamp_sec = (timestamp_ns / 1_000_000_000) as u32;
+        let timestamp_usec = ((timestamp_ns % 1_000_000_000) / 1_000) as u32;
+        (timestamp_sec, timestamp_usec)
+    }
+
+    fn combine_timestamp(timestamp_sec: u32, timestamp_usec: u32) -> u128 {
+        let timestamp_ns = (timestamp_sec as u128 * 1_000_000_000) + (timestamp_usec as u128 * 1_000);
+        timestamp_ns
     }
 }
 
