9690668a878bcec8f3efed4a3c443b0ee6503daa
DrBrad <brad@bradeagle.com>
2025-03-10 20:33:26 -0600

    Data link types

--- src/pcap/inter/pcap_networks.rs
+++ src/pcap/inter/pcap_networks.rs
@@ -2,28 +2,11 @@
 pub enum PcapNetworks {
     Null,
     Ethernet,
-    IEEE802,
-    Arcnet,
-    Slip,
-    Ppp,
-    Fddi,
-    AtmRfc1483,
     Raw,
-    SlipBsdos,
-    PppBsdos,
-    Mpls,
-    Hdlc,
-    LinuxSll,
-    LocalTalk,
-    PfLog,
-    Ieee80211,
-    Arp,
-    Vlan,
-    MplsMcast,
-    Can20b,
     Ipv4,
     Ipv6,
-    BluetoothHciH4
+    BluetoothHciH4,
+    Tun
 }
 
 impl PcapNetworks {
@@ -32,28 +15,11 @@ impl PcapNetworks {
         for c in [
             Self::Null,
             Self::Ethernet,
-            Self::IEEE802,
-            Self::Arcnet,
-            Self::Slip,
-            Self::Ppp,
-            Self::Fddi,
-            Self::AtmRfc1483,
             Self::Raw,
-            Self::SlipBsdos,
-            Self::PppBsdos,
-            Self::Mpls,
-            Self::Hdlc,
-            Self::LinuxSll,
-            Self::LocalTalk,
-            Self::PfLog,
-            Self::Ieee80211,
-            Self::Arp,
-            Self::Vlan,
-            Self::MplsMcast,
-            Self::Can20b,
             Self::Ipv4,
             Self::Ipv6,
-            Self::BluetoothHciH4
+            Self::BluetoothHciH4,
+            Self::Tun
         ] {
             if c.get_code() == code {
                 return Ok(c);
@@ -67,28 +33,11 @@ impl PcapNetworks {
         match self {
             Self::Null => 0,
             Self::Ethernet => 1,
-            Self::IEEE802 => 6,
-            Self::Arcnet => 7,
-            Self::Slip => 8,
-            Self::Ppp => 9,
-            Self::Fddi => 11,
-            Self::AtmRfc1483 => 12,
             Self::Raw => 13,
-            Self::SlipBsdos => 14,
-            Self::PppBsdos => 15,
-            Self::Mpls => 33,
-            Self::Hdlc => 34,
-            Self::LinuxSll => 35,
-            Self::LocalTalk => 36,
-            Self::PfLog => 37,
-            Self::Ieee80211 => 40,
-            Self::Arp => 42,
-            Self::Vlan => 43,
-            Self::MplsMcast => 44,
-            Self::Can20b => 48,
             Self::Ipv4 => 49,
             Self::Ipv6 => 50,
-            Self::BluetoothHciH4 => 108
+            Self::BluetoothHciH4 => 108,
+            Self::Tun => 65534
         }
     }
 
@@ -96,28 +45,11 @@ impl PcapNetworks {
         match self {
             Self::Null => "Null",
             Self::Ethernet => "Ethernet 10MB",
-            Self::IEEE802 => "IEEE 802 Token Ring",
-            Self::Arcnet => "ARCNET",
-            Self::Slip => "Serial Line Internet Protocol",
-            Self::Ppp => "Point-to-Point Protocol",
-            Self::Fddi => "Fiber Distributed Data Interface",
-            Self::AtmRfc1483 => "Asynchronous Transfer Mode",
             Self::Raw => "Raw IP packets",
-            Self::SlipBsdos => "BSD/OS-style SLIP",
-            Self::PppBsdos => "BSD/OS-style PPP",
-            Self::Mpls => "Multi-Protocol Label Switching",
-            Self::Hdlc => "High-Level Data Link Control",
-            Self::LinuxSll => "Linux socket buffer capture format",
-            Self::LocalTalk => "Apple's network",
-            Self::PfLog => "OpenBSD Packet Filter log",
-            Self::Ieee80211 => "IEEE 802.11 Wi-Fi",
-            Self::Arp => "Address Resolution Protocol",
-            Self::Vlan => "IEEE 802.1Q VLAN",
-            Self::MplsMcast => "MPLS multicast",
-            Self::Can20b => "Controller Area Network 2.0B",
             Self::Ipv4 => "IPv4",
             Self::Ipv6 => "IPv6",
-            Self::BluetoothHciH4 => "Bluetooth HCI"
+            Self::BluetoothHciH4 => "Bluetooth HCI",
+            Self::Tun => "Tunnel"
         }.to_string()
     }
 }
