eb71b374e22bf690fb5f87690c9a2563648f8554
DrBrad <brad@bradeagle.com>
2025-03-19 19:32:38 -0600

    MacOS devices

--- src/macos/devices.rs
+++ src/macos/devices.rs
@@ -1,5 +1,51 @@
+use std::io;
+use std::net::IpAddr;
+use crate::packet::inter::data_link_types::DataLinkTypes;
+use crate::packet::layers::ethernet_frame::inter::ethernet_address::EthernetAddress;
 
 #[derive(Clone, Debug)]
 pub struct Device {
+    name: String,
+    address: Option<IpAddr>,
+    index: i32,
+    data_link_type: DataLinkTypes,
+    mac: EthernetAddress,
+    //flags: Vec<InterfaceFlags>
+}
 
+impl Device {
+
+    pub fn list() -> io::Result<Vec<Self>> {
+
+        let devices = Vec::new();
+
+
+        Ok(devices)
+    }
+
+    pub fn get_name(&self) -> String {
+        self.name.clone()
+    }
+
+    pub fn get_address(&self) -> Option<IpAddr> {
+        self.address
+    }
+
+    pub fn get_index(&self) -> i32 {
+        self.index
+    }
+
+    pub fn get_data_link_type(&self) -> DataLinkTypes {
+        self.data_link_type
+    }
+
+    pub fn get_mac(&self) -> EthernetAddress {
+        self.mac
+    }
+
+    /*
+    pub fn get_flags(&self) -> Vec<InterfaceFlags> {
+        self.flags.clone()
+    }
+    */
 }
