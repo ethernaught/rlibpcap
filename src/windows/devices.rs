7f4adce7a0b3880f37d6cda52ad73b0ff0b0628e
DrBrad <brad@bradeagle.com>
2025-04-05 23:41:16 -0600

    Windows

--- src/windows/devices.rs
+++ src/windows/devices.rs
@@ -1,3 +1,4 @@
+use std::io;
 use std::net::IpAddr;
 use crate::packet::layers::ethernet_frame::inter::ethernet_address::EthernetAddress;
 use crate::utils::data_link_types::DataLinkTypes;
@@ -11,4 +12,46 @@ pub struct Device {
     data_link_type: DataLinkTypes,
     mac: EthernetAddress,
     flags: Vec<InterfaceFlags>
+}
+
+impl Device {
+
+    pub fn new(name: String, address: Option<IpAddr>, index: i32, data_link_type: DataLinkTypes, mac: EthernetAddress, flags: Vec<InterfaceFlags>) -> Self {
+        Self {
+            name,
+            address,
+            index,
+            data_link_type,
+            mac,
+            flags
+        }
+    }
+
+    pub fn list() -> io::Result<Vec<Self>> {
+        todo!()
+    }
+
+    pub fn get_name(&self) -> String {
+        self.name.clone()
+    }
+
+    pub fn get_address(&self) -> Option<IpAddr> {
+        self.address
+    }
+
+    pub fn get_index(&self) -> i32 {
+        self.index
+    }
+
+    pub fn get_data_link_type(&self) -> DataLinkTypes {
+        self.data_link_type
+    }
+
+    pub fn get_mac(&self) -> EthernetAddress {
+        self.mac
+    }
+
+    pub fn get_flags(&self) -> Vec<InterfaceFlags> {
+        self.flags.clone()
+    }
 }
