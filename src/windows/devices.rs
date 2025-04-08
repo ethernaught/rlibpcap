a8e338605e9e605b56657d1ea9ecde99f8deb55e
DrBrad <brad@bradeagle.com>
2025-04-07 21:25:13 -0600

    Name unsafe

--- src/windows/devices.rs
+++ src/windows/devices.rs
@@ -46,41 +46,14 @@ impl Device {
         while !adapter.is_null() {
             let mut ar = unsafe { &mut *adapter };
 
-            let fname = unsafe {
-                let fname_ptr = (*adapter).friendly_name;
+            let fname_ptr = ar.friendly_name;
 
-                let mut len = 0;
-                while *fname_ptr.add(len) != 0 {
-                    len += 1;
-                }
+            let mut len = 0;
+            while unsafe { *fname_ptr.add(len) } != 0 {
+                len += 1;
+            }
 
-                String::from_utf16_lossy(slice::from_raw_parts(fname_ptr, len))
-            };
-
-            /*
-            let mut unicast_ptr = ar.first_unicast_address;
-            while !unicast_ptr.is_null() {
-                let unicast = unsafe { &*unicast_ptr };
-                let sockaddr = unsafe { &*unicast.address.lp_sockaddr };
-
-                match sockaddr.sa_family as i32 {
-                    2 => { // AF_INET
-                        let sockaddr_in = unsafe { *(sockaddr as *const _ as *const libc::sockaddr_in) };
-                        let ip = IpAddr::V4(std::net::Ipv4Addr::from(u32::from_be(sockaddr_in.sin_addr.s_addr)));
-                        ip_address = Some(ip);
-                        break;
-                    }
-                    23 => { // AF_INET6
-                        let sockaddr_in6 = unsafe { *(sockaddr as *const _ as *const libc::sockaddr_in6) };
-                        let ip = IpAddr::V6(std::net::Ipv6Addr::from(sockaddr_in6.sin6_addr.s6_addr));
-                        ip_address = Some(ip);
-                        break;
-                    }
-                    _ => {}
-                }
-
-                unicast_ptr = unicast.next;
-            }*/
+            let fname = unsafe { String::from_utf16_lossy(slice::from_raw_parts(fname_ptr, len)) };
 
             let mac = match EthernetAddress::try_from(&ar.physical_address[..ar.physical_address_length as usize]) {
                 Ok(mac) => Some(mac),
