d5efede1eae15b3c7deecb15130026db7ecb763d
DrBrad <brad@bradeagle.com>
2025-04-07 18:40:21 -0600

    Unsafe fix

--- src/windows/devices.rs
+++ src/windows/devices.rs
@@ -32,59 +32,57 @@ impl Device {
     }
 
     pub fn list() -> io::Result<Vec<Self>> {
-        unsafe {
-            let mut buffer_len: ULONG = 15000;
-            let mut buffer: Vec<u8> = vec![0u8; buffer_len as usize];
+        let mut buffer_len: ULONG = 15000;
+        let mut buffer: Vec<u8> = vec![0u8; buffer_len as usize];
 
-            let result = GetAdaptersAddresses(
-                AF_UNSPEC,
-                GAA_FLAG_SKIP_ANYCAST | GAA_FLAG_SKIP_MULTICAST | GAA_FLAG_SKIP_DNS_SERVER,
-                null_mut(),
-                buffer.as_mut_ptr() as PIP_ADAPTER_ADDRESSES_LH,
-                &mut buffer_len,
-            );
+        let result = unsafe { GetAdaptersAddresses(
+            AF_UNSPEC,
+            GAA_FLAG_SKIP_ANYCAST | GAA_FLAG_SKIP_MULTICAST | GAA_FLAG_SKIP_DNS_SERVER,
+            null_mut(),
+            buffer.as_mut_ptr() as PIP_ADAPTER_ADDRESSES_LH,
+            &mut buffer_len,
+        ) };
 
-            if result != 0 {
-                return Err(io::Error::last_os_error());
-            }
+        if result != 0 {
+            return Err(io::Error::last_os_error());
+        }
 
-            let mut adapter = buffer.as_mut_ptr() as PIP_ADAPTER_ADDRESSES_LH;
+        let mut adapter = unsafe { buffer.as_mut_ptr() as PIP_ADAPTER_ADDRESSES_LH };
 
-            let mut devices = Vec::new();
+        let mut devices = Vec::new();
 
-            while !adapter.is_null() {
-                //let uuid = CStr::from_ptr((*adapter).AdapterName).to_string_lossy();
+        while !adapter.is_null() {
+            let mut ar = unsafe { &mut *adapter };
 
+            let fname = unsafe {
                 let fname_ptr = (*adapter).FriendlyName;
+
                 let mut len = 0;
                 while *fname_ptr.add(len) != 0 {
                     len += 1;
                 }
-                let fname_slice = slice::from_raw_parts(fname_ptr, len);
 
-                let mac = match EthernetAddress::try_from(&(*adapter).PhysicalAddress[..(*adapter).PhysicalAddressLength as usize]) {
-                    Ok(mac) => {
-                        Some(mac)
-                    }
-                    Err(_) => {
-                        None
-                    }
-                };
+                OsString::from_wide(slice::from_raw_parts(fname_ptr, len)).to_string_lossy().to_string()
+            };
 
-                devices.push(Self {
-                    name: OsString::from_wide(fname_slice).to_string_lossy().to_string(),
-                    address: None, //UNICAST ADDRESS...
-                    index: (*adapter).IfIndex as i32,
-                    data_link_type: DataLinkTypes::Null,
-                    mac,
-                    flags: InterfaceFlags::from_code((*adapter).Flags)
-                });
+            let mac = match EthernetAddress::try_from(&ar.PhysicalAddress[..ar.PhysicalAddressLength as usize]) {
+                Ok(mac) => Some(mac),
+                Err(_) => None
+            };
 
-                adapter = (*adapter).Next;
-            }
+            devices.push(Self {
+                name: fname,
+                address: None, //UNICAST ADDRESS...
+                index: ar.IfIndex as i32,
+                data_link_type: DataLinkTypes::Null,
+                mac,
+                flags: InterfaceFlags::from_code(ar.Flags as u32)
+            });
 
-            Ok(devices)
+            adapter = unsafe { (*adapter).Next };
         }
+
+        Ok(devices)
     }
 
     pub fn get_name(&self) -> String {
